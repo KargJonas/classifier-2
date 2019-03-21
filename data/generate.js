d5653cbe4a1bb030afec3f2784c897629b4e1df2
Jonas Karg <jonas.karg@hotmail.com>
2019-03-21 22:55:52 +0100

    Training is now possible but not efficient.

--- data/generate.js
+++ data/generate.js
@@ -3,11 +3,13 @@ const generate = require("./generators/generator-lib");
 // const PATH = "dataset-000.json";
 const PATH = "dataset-002.json";
 const AMOUNT = 2000;
+// const AMOUNT = 2;
 const SCALE_X = 1;
 const SCALE_Y = 1;
 
 function f(x) {
-  return 0.5 * Math.pow(x, 2) + 0.1 * Math.pow(x, 3) + 0.1;
+  return 0.5;
+  // return 0.5 * Math.pow(x, 2) + 0.1 * Math.pow(x, 3) + 0.1;
 }
 
 generate({ PATH, AMOUNT, SCALE_X, SCALE_Y, f });
